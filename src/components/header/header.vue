15d4802c69b7e2308f1daed42379ee695f3af285
linhongyu <lhyu606@163.com>
2018-01-12 17:54:33 +0800

    全屏弹窗，评分星星

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -19,12 +19,12 @@
 					<span class="text">{{ seller.supports[0].description}}</span>
 				</div>
 			</div>
-			<div v-if="seller.supports" class="support-count">
+			<div v-if="seller.supports" class="support-count" @click="showDetail">
 				<span class="count">{{ seller.supports.length }}个</span>
 				<i class="icon-keyboard_arrow_right"></i>
 			</div>
 		</div>
-		<div class="bulletin-wrapper">
+		<div class="bulletin-wrapper" @click="showDetail">
 				<span class="bulletin-title"></span><span 
 					class="bulletin-text">{{ seller.bulletin }}</span>
 				<i class="icon-keyboard_arrow_right"></i>
@@ -32,19 +32,60 @@
 		<div class="background">
 			<img :src="seller.avatar" width="100%" height="100%"/>
 		</div>
+		<div v-show="detailShow" class="detail">
+			<div class="detail-wrapper clearfix">
+				<div class="detail-main">
+					<h1 class="name">{{ seller.name}}</h1>
+					<div class="star-wrapper">
+						<star :size="48" :score="seller.score"></star>
+					</div>
+					<div class="title">
+						<div class="line">
+							
+						</div>
+						<div class="text">
+							优惠信息
+						</div>
+						<div class="line">
+							
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="detail-close">
+				<i class="icon-close" @click="hideDetail"></i>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script type="text/ecmascript-6">
 	/* eslint-disable */
+	import star from '@/components/star/star.vue'
 	export default {
 		props: {
 			seller: {
 				type: Object
 			}
 		},
+		data (){
+			return {
+				detailShow: true
+			}
+		},
+		methods:{
+			showDetail(){
+				this.detailShow  = true
+			},
+			hideDetail(){
+				this.detailShow = false
+			}
+		},
 		created(){
 			this.classMap = ['decrease','discount','special','invoice','guarantee']
+		},
+		components: {
+			star
 		}
 	}
 </script>
@@ -165,5 +206,49 @@
 			height: 100%
 			z-index: -1
 			filter: blur(10px)
+		.detail
+			position: fixed
+			z-index: 100
+			top: 0
+			left: 0
+			width: 100%
+			height: 100%
+			overflow: auto
+			background: rgba(7,17,27,0.8)
+			.detail-wrapper
+				min-height: 100%
+				width: 100%
+				.detail-main
+					margin-top:64px
+					padding-bottom: 64px
+					.name
+						line-height: 16px
+						text-align: center
+						font-size: 16px
+						font-weight: 700
+					.star-wrapper
+						margin-top: 18px
+						padding: 2px 0
+						text-align: center
+					.title
+						display: flex
+						width: 80%
+						margin: 28px auto 24px auto
+						.line
+							flex: 1
+							position: relative
+							top: -6px
+							border-bottom: 1px solid rgba(255,255,255,0.2)
+						.text
+							padding: 0 12px 
+							font-weight: 700
+							font-size: 14px
+			.detail-close
+				position: relative
+				width: 32px
+				height: 32px
+				margin: -64px auto 0 auto
+				clear: both
+				font-size: 32px
 
 </style>
