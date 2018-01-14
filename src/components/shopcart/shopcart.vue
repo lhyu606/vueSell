78aae57e127566897d0eb06a0f0e4201d5d7ac1a
lhyu <lhyu606@163.com>
2018-01-14 23:55:16 +0800

    购物车开始

---
+++ src/components/shopcart/shopcart.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="shopcart">
+
+	</div>
+</template>
+
+<script type="ecmascript-6">
+	/* eslint-disable */
+	
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+	
+</style>
